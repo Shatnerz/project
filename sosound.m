bc8b93a2cd83399215be8cb3730bc93d5c09e6a1
Andrew Ahlers <andrew.ahlers@ws339-12.ugrad.physics.mcgill.ca>
2014-04-01 15:34:44 -0400

    b

---
+++ sosound.m
@@ -0,0 +1,22 @@
+%Inputs: handle -- vector from connect() command
+%        distance -- distance between ultrasonic sensor and object
+%Outputs: speed -- speed of sound in m/s
+%         error_Speed -- measurement error on speed of sound
+%         time -- data of time between trigger and echo
+
+function [speed,error_Speed,time]=sosound(handle,distance)
+clear all
+n=50; %number of data points
+
+time=[];
+
+for i=1:n;
+    time(i)=echo_time(handle);
+    pause(.1);
+end
+
+avg_Time=mean(time);
+error_Time=sqrt(var(time));
+
+speed=2*distance/avg_Time*10000;
+error_Speed=2*distance/avg_Time^2*error_Time*10000;
